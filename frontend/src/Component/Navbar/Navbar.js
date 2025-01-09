711e4c26e3e1039d933e1e4d74dbc8583f3448e5
DewminJPH <jphimanshadewmin01@gmail.com>
2025-01-09 19:19:52 +0530

    navbar created

---
+++ frontend/src/Component/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../Assest/Logo.jpg'
+
+export const Navbar = () => {
+  return (
+    <>
+      <div className="navbar">
+        <ul className="nav-menu">
+          <li>Home <hr/></li>
+          <li>About</li>
+          <li>Features</li>
+          <li>How it works</li>
+        </ul>
+        <div className="nav-login">
+          <ul>
+            <li>Login</li>
+          </ul>
+        </div>
+        <div className="nav-signup">
+          <button>Sign Up</button>
+        </div>
+      </div>
+      <div className="nav-logo">
+        <img src={logo} alt="Logo" />
+      </div>
+    </>
+  );
+}
