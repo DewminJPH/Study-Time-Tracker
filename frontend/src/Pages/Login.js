4d20c05bee9dc8bf947db779d572343a5d298d3f
DewminJPH <jphimanshadewmin01@gmail.com>
2025-01-21 23:58:36 +0530

    Login form created

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -1,17 +1,24 @@
-import React from 'react';
+import React,{useState} from 'react';
 import '../Pages/css/Login.css'
 const Login = () => {
+    const[showPassword,setShowPassword]=useState(false);
+    
     return (
         <div className='Login'>
-            <div className='Login-container'>
-                <h4>Welcome back!</h4>
-                <div className='Login-fields'>
-                    <input type="text" placeholder='Username'/>
-                    <input type="password" placeholder='Password'/>
+            <form action='#' method='post' class='login-form'>
+                <h1 class="title">Login</h1>
+                <div class='Username box'>
+                    <input class='field' type='text' id='Username' required/>
+                    <label for='Username'>Username</label>
                 </div>
-                <button>Login</button>
-                <p className="redirtosignup">Don't have an accout yet: <span>sign up</span> here</p>
-            </div>
+                <div class='password box'>
+                    <input class='field' type={showPassword?'text':'password'} id='password' required/>
+                    <label for='password'>Password</label>
+                </div>
+                <div className={showPassword ?'show':'hide'}onClick={()=>setShowPassword(!showPassword)} title={showPassword ? 'Hide Password' : 'Show Password'}>
+                </div>
+                <button class='button' type='submit'>Login</button>
+            </form>
         </div>
     )
 };
